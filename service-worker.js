8ee0497004b7aba6c7d86ae6f0495078142e0933
Madiyan1960 <103851348+Madiyan1960@users.noreply.github.com>
2025-05-04 12:05:09 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,8 @@ self.addEventListener("install", event => {
     caches.open("v1").then(cache => {
       return cache.addAll([
         "/index.html",
-        "/icon.png",
+        "/icon-192.png",
+        "/icon-512.png",
         "/background.jpg"
       ]);
     })
